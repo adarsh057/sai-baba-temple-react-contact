5f90d4fb05dcfcfd46d6a0d6fbfaa9ed6199aaf1
adarsh057 <51473499+adarsh057@users.noreply.github.com>
2025-08-29 08:46:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SaiBabaTempleContact from './components/SaiBabaTempleContact';
+import Sample from './components/Sample';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <SaiBabaTempleContact />
+      <Sample/>
     </div>
   );
 }
