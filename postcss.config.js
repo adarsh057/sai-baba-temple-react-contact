c845e2adb3a68bb20b111dd133277de16e820c5c
adarsh057 <51473499+adarsh057@users.noreply.github.com>
2025-08-29 12:21:59 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    // remove require('tailwindcss'),
-    require('autoprefixer'),
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
