73898076bdb19d8799b283fc028c902f69bde26a
adarsh057 <51473499+adarsh057@users.noreply.github.com>
2025-08-30 06:51:30 +0530

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -396,7 +396,7 @@ const AboutUs = () => {
 
         .page-hero-content {
   max-width: 800px;
-  margin: 50px 10px 10px 10px; 
+  margin: 80px 10px 10px 10px; 
   padding: 0 3rem;
   z-index: 2;
   animation: fadeInUp 1.2s cubic-bezier(0.4, 0, 0.2, 1);
